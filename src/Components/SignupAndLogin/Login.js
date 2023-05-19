b41d11e6b3ff61b21439b704c0bb29f1e6ae821f
thiru1832 <rrthirumugilan18@gmail.com>
2023-05-20 01:25:39 +0530

    added styles for signup page

--- src/Components/SignupAndLogin/Login.js
+++ src/Components/SignupAndLogin/Login.js
@@ -1,9 +1,9 @@
 import { useForm } from "react-hook-form";
-import TextField from "@mui/material/TextField";
+import TextField from '@mui/material/TextField';
 import Button from "@mui/material/Button";
 import NavigationBar from "../NavgationBar/NavigationBar";
-import {Link} from "react-router-dom";
-import "./login.css"
+import { Link } from "react-router-dom";
+import "./login.css";
 
 function Login() {
   const {
@@ -19,72 +19,74 @@ function Login() {
   return (
     <>
       <NavigationBar />
-      <div className="loginouter">
-          <div
-            className="login-inner-div"
-          >
-            <form  className="login-form" onSubmit={handleSubmit(onSubmit)}>
+      <div className="login-outer-div">
+        <div className="login-inner-div">
+          <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
             <div className="form-control">
-              <h1>LOGIN</h1>
+              <h1 className="login-text">LOGIN</h1>
             </div>
-              <div className="form-control">
-                <TextField
-                  id="User-Number-Id"
-                  label="Doctor lisence No/Employee Id*"
-                  placeholder="APH123456"
-                  variant="standard"
-                  {...register("userID", {
-                    required: {
-                      value: true,
-                      message: "User Number is required",
-                    },
-                  })}
-                  error={!!errors.userID}
-                  helperText={errors?.userID?.message}
-                />
-              </div>
+            <div className="form-control">
+              <TextField className="signup-text-field"
+                id="User-Number-Id"
+                label="Doctor lisence No/Employee Id*"
+                placeholder="APH123456"
+                variant="standard"
+                {...register("userID", {
+                  required: {
+                    value: true,
+                    message: "User Number is required",
+                  },
+                })}
+                error={!!errors.userID}
+                helperText={errors?.userID?.message}
+              />
+            </div>
 
-              <div className="form-control">
-                <TextField
-                  id="Password-Id"
-                  label="Password *"
-                  type="password"
-                  variant="standard"
-                  {...register("password", {
-                    required: {
-                      value: true,
-                      message: "password is required",
-                    },
-                  })}
-                  error={!!errors.password}
-                  helperText={errors?.password?.message}
-                />
-              </div>
-              <div className="form-control-password">
-              <Link className="signup-link" to="/signup"> forgot password?</Link> 
-              </div>
+            <div className="form-control">
+              <TextField className="signup-text-field"
+                id="Password-Id"
+                label="Password *"
+                type="password"
+                variant="standard"
+                {...register("password", {
+                  required: {
+                    value: true,
+                    message: "password is required",
+                  },
+                })}
+                error={!!errors.password}
+                helperText={errors?.password?.message}
+              />
+            </div>
+            <div className="form-control-password">
+              <Link className="signup-link" to="/signup">
+                {" "}
+                forgot password?
+              </Link>
+            </div>
 
-              <div className="form-control">
+            <div className="form-control">
               <Button
                 sx={{
                   borderRadius: 8,
                   backgroundColor: "#7EDD6F",
-                  paddingLeft:'60px',
-                  paddingRight:'60px'
+                  paddingLeft: "60px",
+                  paddingRight: "60px",
                 }}
                 variant="contained"
                 type="submit"
               >
                 Login
               </Button>
-              
-              </div>  
-              <div className="form-control">
-              <Link className="signup-link" to="/signup"> Not a member? Register here</Link> 
-              </div>
-                   
-            </form>
-          </div>
+            </div>
+            <div className="form-control">
+              <Link className="signup-link" to="/signup">
+                {" "}
+                Not a member? Register here
+              </Link>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
--- src/Components/SignupAndLogin/Signup.js
+++ src/Components/SignupAndLogin/Signup.js
@@ -1,11 +1,14 @@
 import { useForm } from "react-hook-form";
 import { stateList, countryList, roleList, townCityList } from "./OptionList";
-import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import MenuItem from "@mui/material/MenuItem";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import { Link } from "react-router-dom";
+
 import NavigationBar from "../NavgationBar/NavigationBar";
+import "./signup.css";
 
 function Signup() {
   const {
@@ -21,233 +24,272 @@ function Signup() {
 
   return (
     <>
-     <NavigationBar/>
-      <Box
-        sx={{
-          "& .MuiTextField-root": { m: 1, width: "40ch" },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <div className="signup-div" style={{justifyContent:"center",display:"flex",alignContent:"center"}}>
-          <form onSubmit={handleSubmit(onsubmit)}>
+      <NavigationBar />
+      <div className="signup-outer-div">
+        <div className="signup-inner-div">
+          <Box
+            sx={{
+              "& .MuiTextField-root": { p: "10px", width: "50ch" },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <form onSubmit={handleSubmit(onsubmit)}>
             <div className="form-control">
-              <TextField
-                id="outlined-required"
-                label="Hospital Name *"
-                placeholder="Apollo Hospital"
-                variant="standard"
-                {...register("hospital", {
-                  required: {
-                    value: true,
-                    message: "Hospital Name is required",
-                  },
-                })}
-                error={!!errors.hospital}
-                helperText={errors?.hospital?.message}
-              />
+              <h1 className="login-text">SIGNUP</h1>
             </div>
-
-            <Stack spacing={2} direction="row">
-              <div className="form-control">
+              <div className="sign-up-elements-div">
                 <TextField
-                  id="outlined-select-currency"
-                  select
-                  label="Role *"
+                  className="signup-text-field"
+                  id="outlined-required"
+                  label="Hospital Name *"
+                  placeholder="Apollo Hospital"
                   variant="standard"
-                  defaultValue=""
-                  {...register("role", {
+                  {...register("hospital", {
                     required: {
                       value: true,
-                      message: "Role is required",
+                      message: "Hospital Name is required",
                     },
                   })}
-                  error={!!errors.role}
-                  helperText={errors?.role?.message}
-                >
-                  {roleList.map((option) => (
-                    <MenuItem key={option.value} value={option.value}>
-                      {option.label}
-                    </MenuItem>
-                  ))}
-                </TextField>
-              </div>
-              <div className="form-control">
-                <TextField
-                  id="User-Number-Id"
-                  label="Doctor lisence No/Employee Id*"
-                  placeholder="APH123456"
-                  variant="standard"
-                  {...register("userID", {
-                    required: {
-                      value: true,
-                      message: "User Number is required",
-                    },
-                  })}
-                  error={!!errors.userID}
-                  helperText={errors?.userID?.message}
-                />
-              </div>
-            </Stack>
-
-            <Stack spacing={2} direction="row">
-              <div className="form-control">
-                <TextField
-                  id="First-Name-Id"
-                  label="First Name *"
-                  placeholder="Virat"
-                  variant="standard"
-                  {...register("firstName", {
-                    required: {
-                      value: true,
-                      message: "First Name is required",
-                    },
-                  })}
-                  error={!!errors.firstName}
-                  helperText={errors?.firstName?.message}
+                  error={!!errors.hospital}
+                  helperText={errors?.hospital?.message}
                 />
               </div>
 
-              <div className="form-control">
+              <Stack spacing={2} direction="row">
+                <div className="sign-up-elements-div">
+                  <TextField
+                    className="signup-text-field"
+                    id="outlined-select-currency"
+                    select
+                    label="Role *"
+                    variant="standard"
+                    defaultValue=""
+                    {...register("role", {
+                      required: {
+                        value: true,
+                        message: "Role is required",
+                      },
+                    })}
+                    error={!!errors.role}
+                    helperText={errors?.role?.message}
+                  >
+                    {roleList.map((option) => (
+                      <MenuItem key={option.value} value={option.value}>
+                        {option.label}
+                      </MenuItem>
+                    ))}
+                  </TextField>
+                </div>
+                <div className="sign-up-elements-div">
+                  <TextField
+                    className="signup-text-field"
+                    id="User-Number-Id"
+                    label="Doctor lisence No/Employee Id*"
+                    placeholder="APH123456"
+                    variant="standard"
+                    {...register("userID", {
+                      required: {
+                        value: true,
+                        message: "User Number is required",
+                      },
+                    })}
+                    error={!!errors.userID}
+                    helperText={errors?.userID?.message}
+                  />
+                </div>
+              </Stack>
+
+              <Stack spacing={2} direction="row">
+                <div className="sign-up-elements-div">
+                  <TextField
+                    className="signup-text-field"
+                    id="First-Name-Id"
+                    label="First Name *"
+                    placeholder="Virat"
+                    variant="standard"
+                    {...register("firstName", {
+                      required: {
+                        value: true,
+                        message: "First Name is required",
+                      },
+                    })}
+                    error={!!errors.firstName}
+                    helperText={errors?.firstName?.message}
+                  />
+                </div>
+
+                <div className="sign-up-elements-div">
+                  <TextField
+                    className="signup-text-field"
+                    id="Last-Name-Id"
+                    label="Last Name"
+                    placeholder="Kholi"
+                    variant="standard"
+                    {...register("lastName")}
+                  />
+                </div>
+              </Stack>
+
+              <Stack spacing={2} direction="row">
+                <div className="sign-up-elements-div">
+                  <TextField
+                    className="signup-text-field"
+                    id="Country-ID"
+                    select
+                    label="Country"
+                    variant="standard"
+                    defaultValue=""
+                    {...register("country")}
+                  >
+                    {countryList.map((option) => (
+                      <MenuItem key={option.value} value={option.value}>
+                        {option.label}
+                      </MenuItem>
+                    ))}
+                  </TextField>
+                </div>
+
+                <div className="sign-up-elements-div">
+                  <TextField
+                    className="signup-text-field"
+                    id="State-ID"
+                    select
+                    label="State"
+                    variant="standard"
+                    defaultValue=""
+                    {...register("state")}
+                  >
+                    {stateList.map((option) => (
+                      <MenuItem key={option.value} value={option.value}>
+                        {option.label}
+                      </MenuItem>
+                    ))}
+                  </TextField>
+                </div>
+              </Stack>
+
+              <Stack spacing={2} direction="row">
+                <div className="sign-up-elements-div">
+                  <TextField
+                    className="signup-text-field"
+                    id="Town-City-ID"
+                    select
+                    label="Town/City"
+                    variant="standard"
+                    defaultValue=""
+                    {...register("townCity")}
+                  >
+                    {townCityList.map((option) => (
+                      <MenuItem key={option.value} value={option.value}>
+                        {option.label}
+                      </MenuItem>
+                    ))}
+                  </TextField>
+                </div>
+                <div className="sign-up-elements-div">
+                  <TextField
+                    className="signup-text-field"
+                    id="Pincode-Id"
+                    label="Pincode"
+                    placeholder="000123"
+                    variant="standard"
+                    {...register("pincode")}
+                  />
+                </div>
+              </Stack>
+
+              <div className="sign-up-elements-div">
                 <TextField
-                  id="Last-Name-Id"
-                  label="Last Name"
-                  placeholder="Kholi"
+                  className="signup-text-field"
+                  id="Email-Id"
+                  label="Email Id *"
+                  placeholder="abc123@gmail.com"
                   variant="standard"
-                  {...register("lastName")}
+                  {...register("emailId", {
+                    required: {
+                      value: true,
+                      message: "Email ID is required",
+                    },
+                    pattern: {
+                      value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+                      message: "Enter valid email id",
+                    },
+                  })}
+                  error={!!errors.emailId}
+                  helperText={errors?.emailId?.message}
                 />
               </div>
-            </Stack>
 
-            <Stack spacing={2} direction="row">
-              <div className="form-control">
-                <TextField
-                  id="Country-ID"
-                  select
-                  label="Country"
-                  variant="standard"
-                  defaultValue=""
-                  {...register("country")}
+              <Stack spacing={2} direction="row">
+                <div className="sign-up-elements-div">
+                  <TextField
+                    className="signup-text-field"
+                    id="Password-Id"
+                    label="Password *"
+                    type="password"
+                    variant="standard"
+                    {...register("password", {
+                      required: {
+                        value: true,
+                        message: "password is required",
+                      },
+                    })}
+                    error={!!errors.password}
+                    helperText={errors?.password?.message}
+                  />
+                </div>
+
+                <div className="sign-up-elements-div">
+                  <TextField
+                    className="signup-text-field"
+                    id="Confirm-Password-Id"
+                    label="Comfirm Password *"
+                    type="password"
+                    variant="standard"
+                    {...register("confirmPassword", {
+                      required: {
+                        value: true,
+                        message: "password is required",
+                      },
+                      validate: (val) => {
+                        if (watch("password") !== val) {
+                          return "Password should match";
+                        }
+                      },
+                    })}
+                    error={!!errors.confirmPassword}
+                    helperText={errors?.confirmPassword?.message}
+                  />
+                </div>
+              </Stack>
+
+              <div className="signup-btn">
+                <Button
+                  sx={{
+                    borderRadius: 20,
+                    backgroundColor: "#7EDD6F",
+                    justifyContent: "center",
+                    paddingLeft: "60px",
+                  paddingRight: "60px",
+                  }}
+                  variant="contained"
+                  type="submit"
                 >
-                  {countryList.map((option) => (
-                    <MenuItem key={option.value} value={option.value}>
-                      {option.label}
-                    </MenuItem>
-                  ))}
-                </TextField>
+                  SIGN UP NOW
+                </Button>
               </div>
 
               <div className="form-control">
-                <TextField
-                  id="State-ID"
-                  select
-                  label="State"
-                  variant="standard"
-                  defaultValue=""
-                  {...register("state")}
-                >
-                  {stateList.map((option) => (
-                    <MenuItem key={option.value} value={option.value}>
-                      {option.label}
-                    </MenuItem>
-                  ))}
-                </TextField>
-              </div>
-            </Stack>
-
-            <div className="form-control">
-              <TextField
-                id="Town-City-ID"
-                select
-                label="Town/City"
-                variant="standard"
-                defaultValue=""
-                {...register("townCity")}
-              >
-                {townCityList.map((option) => (
-                  <MenuItem key={option.value} value={option.value}>
-                    {option.label}
-                  </MenuItem>
-                ))}
-              </TextField>
+              <Link className="signup-link" to="/login">
+                {" "}
+                Already a member?lets login!
+              </Link>
             </div>
-
-            <div className="form-control">
-              <TextField
-                id="Email-Id"
-                label="Email Id *"
-                placeholder="abc123@gmail.com"
-                variant="standard"
-                {...register("emailId", {
-                  required: {
-                    value: true,
-                    message: "Email ID is required",
-                  },
-                  pattern: {
-                    value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
-                    message: "Enter valid email id",
-                  },
-                })}
-                error={!!errors.emailId}
-                helperText={errors?.emailId?.message}
-              />
-            </div>
-
-            <Stack spacing={2} direction="row">
-              <div className="form-control">
-                <TextField
-                  id="Password-Id"
-                  label="Password *"
-                  type="password"
-                  variant="standard"
-                  {...register("password", {
-                    required: {
-                      value: true,
-                      message: "password is required",
-                    },
-                  })}
-                  error={!!errors.password}
-                  helperText={errors?.password?.message}
-                />
-              </div>
-
-              <div className="form-control">
-                <TextField
-                  id="Confirm-Password-Id"
-                  label="Comfirm Password *"
-                  type="password"
-                  variant="standard"
-                  {...register("confirmPassword", {
-                    required: {
-                      value: true,
-                      message: "password is required",
-                    },
-                    validate: (val) => {
-                      if (watch("password") !== val) {
-                        return "Password should match";
-                      }
-                    },
-                  })}
-                  error={!!errors.confirmPassword}
-                  helperText={errors?.confirmPassword?.message}
-                />
-              </div>
-            </Stack>
-
-            <Button
-              style={{
-                borderRadius: 10,
-                backgroundColor: "#7EDD6F",
-                justifyContent:"center"
-              }}
-              variant="contained"
-              type="submit"
-            >
-              Sign Up
-            </Button>
-          </form>
+            </form>
+          </Box>
         </div>
-      </Box>
+      </div>
     </>
   );
 }
