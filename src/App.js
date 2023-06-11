2a19974e95300c8dd57ecc31e725f2f653baaf4d
thiru1832 <rrthirumugilan18@gmail.com>
2023-06-11 18:28:10 +0530

    restricted the route based on profile

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import HomePage from "./component/home/home-page";
-import Signup from "./component/authenticate/sign-up"
-import Login from "./component/authenticate/login"
+import Signup from "./component/authenticate/sign-up";
+import Login from "./component/authenticate/login";
 import NoRoute from "./component/no-route";
 import AboutUs from "./component/aboutUs/about-us";
 import ContactUs from "./component/contactUs/contact-us";
@@ -8,50 +8,94 @@ import ReceptionistDashboard from "./component/dashboard/reception/reception-das
 import PatientRegistration from "./component/dashboard/reception/patient-registration";
 import DoctorDashboard from "./component/dashboard/doctor/doctor-dashboard";
 import NurseDashboard from "./component/dashboard/nurse/nurse-dashboard";
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import wrapper from "./component/layoutHOC/layout";
 import PatientRegistrationTest from "./component/dashboard/reception/patient-registration";
 import ReceptionistDashboardTest from "./component/dashboard/reception/reception-dashboard";
 import PatientTask from "./component/dashboard/doctor/patient-task";
 import NurseDashboardTest from "./component/dashboard/nurse/nurse-dashboard-new";
 import Loader from "./component/master/loader";
-
-
-
+import { useSelector } from "react-redux";
 
 function App() {
-
   const ReceptionistDashboardWrapper = wrapper(ReceptionistDashboard);
   const PatientRegistrationWrapper = wrapper(PatientRegistration);
   const DoctorDashboardWrapper = wrapper(DoctorDashboard);
   const PatientTaskWrapper = wrapper(PatientTask);
   const NurseDashboardWrapper = wrapper(NurseDashboardTest);
 
-  return(
-  <>
-  <Router>
-    <Routes>
-      <Route path="/" element={<HomePage/>}/>
-      <Route path="/signup" element={<Signup/>}/>
-      <Route path="/login" element={<Login/>}/>
-      <Route path = "/aboutus" element={<AboutUs/>}/>
-      <Route path="/contactus" element={<ContactUs/>}/>
-      <Route path="/receptionist" element={<ReceptionistDashboardWrapper/>}/>
-      <Route path="/doctor" element={<DoctorDashboardWrapper/>}/>
-      <Route path="/patientRegistration" element={<PatientRegistrationWrapper />} />
-      <Route path="/patientRegistration/:id" element={<PatientRegistrationWrapper />} />
-      <Route path= "/patientTaskUpdate/:id" element={<PatientTaskWrapper/>}/>
-      <Route path = "/nurse" element={<NurseDashboard/>}/>
-      <Route path = "/nurseDashTest" element={<NurseDashboardWrapper/>}/>
-      <Route path ="/loader" element ={<Loader/>}/>
-      {/* <Route path="/receptionistDashboard" element = {<ReceptionistDashboardTestWrapper/>}/> */}
-      {/* <Route path="/patientRegistrationTest" element ={<PatientRegistrationTest/>} />
-      <Route path="/patientRegistrationTest/:id" element={<PatientRegistrationTest />} /> */}
-      <Route path="*" element = {<NoRoute/>}/>
-    </Routes>
-  </Router>
-  </>
-  )
+  const role = useSelector((state) => state.role);
+  const isAuthenticated = useSelector((state) => state.role);
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/contactus" element={<ContactUs />} />
+
+          {!isAuthenticated ? (
+            <>
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/login" element={<Login />} />
+            </>
+          ) : (
+            <></>
+          )}
+
+          {isAuthenticated && role === "Receptionist" ? (
+            <>
+              <Route
+                path="/receptionist"
+                element={<ReceptionistDashboardWrapper />}
+              />
+              <Route
+                path="/patientRegistration"
+                element={<PatientRegistrationWrapper />}
+              />
+              <Route
+                path="/patientRegistration/:id"
+                element={<PatientRegistrationWrapper />}
+              />
+            </>
+          ) : (
+            <></>
+          )}
+
+          {isAuthenticated && role === "Doctor" ? (
+            <>
+              <Route path="/doctor" element={<DoctorDashboardWrapper />} />
+              <Route
+                path="/patientTaskUpdate/:id"
+                element={<PatientTaskWrapper />}
+              />
+            </>
+          ) : (
+            <></>
+          )}
+          {isAuthenticated && role === "Nurse" ? (
+            <>
+              <Route
+                path="/nurseDashTest"
+                element={<NurseDashboardWrapper />}
+              />
+            </>
+          ) : (
+            <></>
+          )}
+
+          <Route path="/nurse" element={<NurseDashboard />} />
+
+          {/* <Route path="/loader" element={<Loader />} /> */}
+          {/* <Route path="/receptionistDashboard" element = {<ReceptionistDashboardTestWrapper/>}/> */}
+          {/* <Route path="/patientRegistrationTest" element ={<PatientRegistrationTest/>} />
+            <Route path="/patientRegistrationTest/:id" element={<PatientRegistrationTest />} /> */}
+          <Route path="*" element={<NoRoute />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
