d907bf4f2a739f63e39653de06d9258f97548de2
sagarbehare <sagarbehare10@gmail.com>
2023-06-15 22:03:51 +0530

    doctor dashboard UI changes.

--- src/component/dashboard/doctor/doctor-dashboard.js
+++ src/component/dashboard/doctor/doctor-dashboard.js
@@ -1,122 +1,126 @@
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
+import "./doctor-dashboard.css";
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
 import axios from "axios";
 import { API_URL } from "../../../utils/constant";
 import moment from "moment";
-import "./doctor-dashboard.css";
-import TextField from "@mui/material/TextField";
+import MaterialTable from "material-table";
+import { ThemeProvider, createTheme } from "@mui/material";
+import tableIcons from "../../master/MaterialTableIcons";
 
 export default function DoctorDashboard() {
-  const [filterText, setFilterText] = useState("");
-  const [rows,setRow]= useState([]);
+  const defaultMaterialTheme = createTheme();
+  const [data, setData] = useState([]);
   const hospitalName = localStorage.getItem('hospitalName');
-
-  const filteredData = rows.filter((item) =>
-    item.patientName.toLowerCase().includes(filterText.toLowerCase())
-  );
-
-  const handleFilter = (event) => {
-    setFilterText(event.target.value);
-  };
-
   
   useEffect(() => {
-    console.log("API_URL : " + API_URL);
-
     axios
       .get(API_URL + `/patientRegistration/listPatient/${hospitalName}`)
 
       .then((response) => {
         console.log(response.data);
-        setRow(response.data);
+        setData(response.data);
       })
       .catch((error) => {
         console.error(error);
       });
   }, []);
 
+  const columns = [
+    { title: "MRN", field: "mrn",
+      cellStyle: {
+        width: "400px"
+      },
+      render: rowData => (
+          <a href={`/patientTaskUpdate/${rowData._id}`} rel="noopener noreferrer">
+            {rowData.mrn}
+          </a>
+      )
+    },
+    { title: "Patient Name", field: "patientName",
+      cellStyle: {
+        width: "500px"
+      }
+    },
+    { title: "Ward", field: "ward",
+      cellStyle: {
+        width: "300px"
+      }
+    },
+    { title: "Room", field: "room",
+      cellStyle: {
+        width: "100px"
+      }
+    },
+    { title: "Bed", field: "bed",
+      cellStyle: {
+        width: "100px"
+      }
+    },
+    { title: "Admission Date", field: "admissionDate",
+      cellStyle: {
+        width: "400px"
+      },
+      render: (rowData) => {
+        return(
+        <>
+          {moment(rowData.admissionDate).format("YYYY-MM-DD")}
+        </>
+      )}
+    },
+    { title: "Status", field: "status",
+      cellStyle: {
+        width: "100px"
+      },
+      render: row => (
+        <>
+        {(row.status === "ADMITTED") ? (
+            <div style={{ backgroundColor: "#54B435", color: "white", borderRadius: "5px", textAlign: "center", padding: "7px", width: "85px", fontFamily: "Roboto, Helvetica, Arial, sans-serif", fontWeight: "500", fontSize: "0.875rem" }}>
+              {row.status.toUpperCase()}
+            </div>  
+        ) : (
+          <div style={{ backgroundColor: "#E57C23", color: "white", borderRadius: "5px", textAlign: "center", padding: "7px", width: "85px", fontFamily: "Roboto, Helvetica, Arial, sans-serif", fontWeight: "500", fontSize: "0.875rem" }}>
+            {row.status.toUpperCase()}
+          </div>
+        )
+        }
+        </>
+      )
+    }
+  ];
+
+
   return (
     <>
-      <div className="reception-table-outer-div">
-      <TextField sx={{size:'10px',margin:'50px 0 50px 0',background:'white',width:'40%'}}
-        value={filterText}
-        onChange={handleFilter}
-        label="Search Name"
-        placeholder="Search Name"
-        variant="outlined"
-      />
-        <div className="reception-table-inner-div">
-          
-          <TableContainer sx={{ borderRadius: "30px" }} component={Paper}>
-            <Table
-              sx={{ minWidth: 400 }}
-              size="small"
-              aria-label="a dense table"
-              initialState={{
-                pagination: {
-                  paginationModel: { page: 0, pageSize: 5 },
+      <div
+          style={{
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <div
+          style={{
+            width: "90%",
+            alignItems: "center",
+            justifyContent: "center",
+            marginTop: "50px",
+          }}
+        >
+          <ThemeProvider theme={defaultMaterialTheme}>
+            <MaterialTable
+              icons={tableIcons}
+              title={""}
+              columns={columns}
+              data={data}
+              options={{
+                headerStyle: {
+                  backgroundColor: "#27a2ee",
+                  color: "#FFF",
                 },
               }}
-              pageSizeOptions={[5, 10]}
-            >
-              <TableHead>
-                <TableRow sx={{ background: "#4545453e" }}>
-                  <TableCell align="center">MRN</TableCell>
-                  <TableCell align="center">PATIENT NAME</TableCell>
-                  <TableCell align="center">WARD</TableCell>
-                  <TableCell align="center">ROOM NO</TableCell>
-                  <TableCell align="center">BED</TableCell>
-                  <TableCell align="center">STATUS</TableCell>
-                  <TableCell align="center">ADMISSION DATE</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody sx={{ background: "#f1f1f1b6" }}>
-                {filteredData.map((row) => (
-                  <TableRow key={row._id}>
-                    <TableCell
-                      sx={{
-                        fontStyle: "oblique",
-                        fontSize: "15px",
-                        fontWeight: "fontWeightMedium",
-                      }}
-                      align="center"
-                      component="a"
-                      href={`/patientTaskUpdate/${row._id}`}
-                    >
-                      {row.mrn}{" "}
-                    </TableCell>
-
-                    <TableCell align="center">{row.patientName}</TableCell>
-                    <TableCell align="center">{row.ward}</TableCell>
-                    <TableCell align="center">{row.room}</TableCell>
-                    <TableCell align="center">{row.bed}</TableCell>
-
-                    {row.status === "ADMITTED" ? (
-                      <TableCell sx={{ color: "white" }} align="center">
-                        <p className="admitted-status-div">{row.status} </p>
-                      </TableCell>
-                    ) : (
-                      <TableCell sx={{ color: "white" }} align="center">
-                        <p className="discharged-status-div">{row.status} </p>
-                      </TableCell>
-                    )}
-
-                    <TableCell align="center">
-                      {moment(row.admissionDate).format("YYYY-MM-DD")}
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
+            />
+          </ThemeProvider>
         </div>
       </div>
     </>
