92e8ef82b6b3a6909e716b0b9d8f9232ff4600e4
thiru1832 <rrthirumugilan18@gmail.com>
2023-06-16 23:10:10 +0530

    contact Us image

--- src/component/aboutUs/about-us.js
+++ src/component/aboutUs/about-us.js
@@ -1,10 +1,14 @@
 import NavigationBar from "../navgation/navigation-bar";
+import aboutUs from '../../images/AboutUs.png'
 
 function AboutUs(){
     return(
         <>
         <NavigationBar/>
-        <h1> About Us page - development in progress</h1>
+        <div style={{height:'100vh',width:'100vh'}}>
+        <img  src={aboutUs} alt="This is about Us"></img>
+        </div>
+        
         </>
     )
 }
